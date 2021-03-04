c3d1afd451b804a1630b683731f6bf23f51d48bc
Oliver Inglis <ollie@theinglistwins.com>
2021-03-04 17:29:06 +0000

    config added

--- src/views/ThreeGraph.vue
+++ src/views/ThreeGraph.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
-    <p>This is a 3d interactive version of Monika's animation. The student can interactive with the 3d model on their own. Work in progress.</p>
+    <p>
+    1) can we add on both animations a vector along z to denote the wmega vector? so that it appears to the left of the wmega symbol
+    and 2) and can we do a vector for the velocity of point B too? so that it is also tangent to its circular motion
+</p>
     <iframe src="https://player.vimeo.com/video/515639286" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
 <p><a href="https://vimeo.com/515639286">Animation</a> from <a href="https://vimeo.com/twinldn">TWIN</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
     <three-2 />
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: '/imp-vis-research/'
+  }
